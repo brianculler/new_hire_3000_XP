831fdd7f917d274b2e3af5aa8d8ada518953303e
Jiayuan Chen <jiayuan.chen@oracle.com>
2013-04-02 12:01:46 -0400

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ NewHire3000Xp::Application.routes.draw do
   get "static_pages/home"
 
   get "static_pages/help"
+  
+  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
