a1779ccc2e6e68bcdd5f5ff4e6202ef1850da5a9
Jiayuan Chen <jiayuan.chen@oracle.com>
2013-04-02 11:44:59 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewHire3000Xp::Application.config.secret_token = '3081393a477fbddb0c475da683b596eba4fab564e4f0ad01ae0355fd64176d6f75ca398d5414b8aaae967dba78feb19ba1da44ffa535c9a1bcf3b5bc3a8a1780'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NewHire3000Xp::Application.config.session_store :cookie_store, key: '_new_hire_3000_xp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NewHire3000Xp::Application.config.session_store :active_record_store
