ae315c0d8d1c2bdc117773aa3dd5bf30b56fc13d
Jiayuan Chen <jiayuan.chen@oracle.com>
2013-04-25 14:06:01 -0400

    modified email text

--- app/mailers/new_hire_mailer.rb
+++ app/mailers/new_hire_mailer.rb
@@ -9,41 +9,37 @@ class NewHireMailer < ActionMailer::Base
 
   def laptop_email(new_hire)
   	@new_hire = new_hire
-  	mail(:to => new_hire.email, :subject => "New Laptop for #{new_hire.name}!")
+  	mail(:to => new_hire.email, :subject => "New Laptop for #{new_hire.name}")
   end
 
   def badge_email(new_hire)
   	@new_hire = new_hire
-  	mail(:to => new_hire.email, :subject => "New Keycard for #{new_hire.name}!")
+  	mail(:to => new_hire.email, :subject => "Keycard Badge for #{new_hire.name}")
   end
   
   def docs_email(new_hire)
     @new_hire = new_hire
-    mail(:to => new_hire.email, :subject => "Documents for #{new_hire.name}!")
+    mail(:to => new_hire.email, :subject => "Documents for #{new_hire.name}")
   end
 
   def IRC_email(new_hire)
     @new_hire = new_hire
-    mail(:to => new_hire.email, :subject => "IRC for #{new_hire.name}!")
+    mail(:to => new_hire.email, :subject => "IRC for #{new_hire.name}")
   end
 
   def seat_email(new_hire)
     @new_hire = new_hire
-    mail(:to => new_hire.email, :subject => "New seat for #{new_hire.name}!")
+    mail(:to => new_hire.email, :subject => "New seat for #{new_hire.name}")
   end
 
   def VM_email(new_hire)
     @new_hire = new_hire
-    mail(:to => new_hire.email, :subject => "VM for #{new_hire.name}!")
+    mail(:to => new_hire.email, :subject => "VM for #{new_hire.name}")
   end
 
   def VPN_email(new_hire)
     @new_hire = new_hire
-    mail(:to => new_hire.email, :subject => "VPN Access for #{new_hire.name}!")
-  end
-  
-  def email_2(address)
-    mail(:to => address, :subject => "laptop email 2")
+    mail(:to => new_hire.email, :subject => "VPN Access for #{new_hire.name}")
   end
   
 end
